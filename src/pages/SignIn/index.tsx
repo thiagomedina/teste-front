9f4cacfd1534c7cc202b71690c3f993eb133f4bd
thiagomedina <thiagomedina001@gmail.com>
2020-10-05 20:30:54 -0300

    created validation form file with yup

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,19 @@
-import React, { useRef, useCallback } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { FormHandles } from '@unform/core';
-import { Form } from '@unform/web';
-// import * as Yup from 'yup';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useRef, useCallback } from "react";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FormHandles } from "@unform/core";
+import { Form } from "@unform/web";
+import { Link, useHistory } from "react-router-dom";
+import * as Yup from 'yup';
+
 
 // import { useAuth } from '../../hooks/auth';
 // import { useToast } from '../../hooks/toast';
 // import getValidationErrors from '../../utils/getValidationErrors';
-import logoImg from '../../assets/logo.png';
-import Button from '../../components/Button';
-import Input from '../../components/Input';
+import logoImg from "../../assets/logo.png";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
 
-import { Container, Content, AnimationContainer, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from "./styles";
 
 interface SignInFormData {
   email: string;
@@ -24,20 +25,17 @@ const SignIn: React.FC = () => {
 
   const history = useHistory();
 
-
   const handleSubmit = () => {
-    console.log('foi')
-  }
+    console.log("foi");
+  };
 
   return (
     <Container>
       <Content>
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
-
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Faça seu login</h1>
-
             <Input name="email" icon={FiMail} placeholder="Email" />
             <Input
               name="password"
@@ -45,9 +43,7 @@ const SignIn: React.FC = () => {
               type="password"
               placeholder="Senha"
             />
-
             <Button type="submit"> Entrar </Button>
-
           </Form>        
         </AnimationContainer>
       </Content>
