b7f8d763c1aa3ea9005d3af3dd8b740497af566d
thiagomedina <thiagomedina001@gmail.com>
2021-07-06 18:32:29 -0300

    cleaned reposotory

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-});
-
-export const apiViaCep = axios.create({ baseURL: 'https://viacep.com.br/ws/' });
